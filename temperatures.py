0e96e3c6856f801207a188731c8aa151b0d1b270
joaopedrocastilhocardoso <95507750+joaopedrocastilhocardoso@users.noreply.github.com>
2024-09-20 12:50:55 -0300

    Added file on conversions on temperature units
    Learning variables and format strings

---
+++ temperatures.py
@@ -0,0 +1,4 @@
+celsius_temp = 40
+fahrenheit_temp = (celsius_temp*9/5)+32
+
+print(f"The temperature given in Celsius is {celsius_temp}, the correlated temperature in Fahrenheit is {fahrenheit_temp}, and in Kelvin, {celsius_temp + 273}")
